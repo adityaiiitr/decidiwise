8e97ef11b3c91acd09372d0d690a14aa472dc0e7
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2024-02-10 19:46:34 +0530

    initial setup

---
+++ app/(user)/[userId]/(user)/layout.tsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+
+export default async function RootLayout({
+    children
+}:{
+    children : React.ReactNode;
+}){
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
